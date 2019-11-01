7a8ca633836c73c59139691f0a90538836ad1f2e
Utkichaps <utkarshchhapekar2@gmail.com>
2019-11-01 19:37:21 +0530

    Arcade Mode added

---
+++ src/mazeducks/ArcadePlayer.java
@@ -0,0 +1,42 @@
+package mazeducks;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+public class ClassicPlayer extends JPanel{
+	int x, y;
+	
+    public ClassicPlayer() {
+    	this.setBackground(Color.getHSBColor(0.3f, 0.3f, 1));
+    	this.setSize(ClassicMaze.panelSize, ClassicMaze.panelSize);
+    }
+
+    public void moveLeft() {
+    	if((x > 0) && (ClassicMaze.map[x-1][y] == 1 || ClassicMaze.map[x-1][y] == 3 || ClassicMaze.map[x-1][y] == 2)){
+	    	this.setLocation(this.getX()-25, this.getY());
+	    	x--;
+    	}
+    }
+
+    public void moveRight() {
+    	if(x < ClassicMaze.columns-1 && (ClassicMaze.map[x+1][y] == 1 || ClassicMaze.map[x+1][y] == 3 || ClassicMaze.map[x+1][y] == 2)){
+	    	this.setLocation(this.getX()+25, this.getY());
+	    	x++;
+    	}
+    }
+
+    public void moveUp() {
+    	if(y > 0 && (ClassicMaze.map[x][y-1] == 1 || ClassicMaze.map[x][y-1] == 3 || ClassicMaze.map[x][y-1] == 2)){
+	    	this.setLocation(this.getX(), this.getY()-25);
+	    	y--;
+    	}
+    }
+
+    public void moveDown() {
+    	if(y < ClassicMaze.rows-1 && (ClassicMaze.map[x][y+1] == 1 || ClassicMaze.map[x][y+1] == 3 || ClassicMaze.map[x][y+1] == 2)){
+	    	this.setLocation(this.getX(), this.getY()+25);
+	    	y++;
+    	}
+    }
+}
